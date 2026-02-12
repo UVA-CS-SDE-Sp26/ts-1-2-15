b90963c0a40cdb760c2e11d1de5af64628b0bc44
Arnav Jadhav <107277606+arnavjadhav@users.noreply.github.com>
2026-02-12 13:25:54 -0500

    Add main method to TopSecret class

--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -1,6 +1,11 @@
 /**
  * Commmand Line Utility
  */
-public class TopSecret {
 
+public class TopSecret{
+    public static void main(String[] args){
+        ProgramControl control = new ProgramControl();
+        Userinterface ui = new Userinterface(control);
+        ui.run(args);
+    }
 }
